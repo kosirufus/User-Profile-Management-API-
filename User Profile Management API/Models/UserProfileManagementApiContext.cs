c16c13683502365d3fb895ec16b2a6abe029680c
Ezeagu Kosi <kosialex66@gmail.com>
2026-01-13 01:55:12 +0100

    added UserProfileManagementApiContext (Database context)

---
+++ User Profile Management API/Models/UserProfileManagementApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace User_Profile_Management_API.Models
+{
+    public class UserProfileManagementApiContext : DbContext
+    {
+        /* DbContextOptions (a class provided by EF Core) holds configuration settings for  UserProfileManagementApiContext */
+        public UserProfileManagementApiContext (DbContextOptions<UserProfileManagementApiContext> options) : base(options)
+        {
+        }        
+
+        public DbSet<User> Users { get; set; } = null!;
+        /* null! shows that Users won't actually be empty at runtime.
+         * DbSet <T> represents a collection of entities of type T in the database
+         where my T == User which is my entity model*/
+
+    }
+}
